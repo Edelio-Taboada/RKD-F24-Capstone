657253ff5ba8b6696348117bb1d20595fc622ab4
Sophia <sophiaho@andrew.cmu.edu>
2024-11-16 22:32:03 -0500

    Added LERP for checkpoint

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@
     Your main fucntion can take in different command line arguments to run different parts of your code.
 """
 import argparse
+import numpy as np
+import utils
+from motion_planner import TrajectoryFollower
+from frankapy import FrankaArm
 
 # Define default values
 parser = argparse.ArgumentParser()
@@ -20,3 +24,33 @@ args.foo = 2
 args.bar = 3
 
 # Call the program with passed arguments
+
+MAX_ACCELERATIONS = [15, 7.5, 10, 12.5, 15, 20, 20]
+SAFE_MAX_ACCELERATIONS = [15, 7.5, 10, 12.5, 15, 20, 20]
+
+times = np.array([[5,10]])
+
+ON_MARKER = [0.15444055, 0.29287314, 0.30672833, -2.38176281, -0.40874126, 2.7013571, 1.98458884]
+ABOVE_MARKER = [0.16167332, -0.14580426, 0.37693924, -2.44870918, -0.15881228, 2.47492598, 1.78272693]
+HOME_POS = [5.35584243e-04, -7.85295146e-01, 2.69296343e-04, -2.35705560e+00, -4.33021669e-04, 1.57136411e+00, 7.85855047e-01]
+
+q0 = np.array(HOME_POS)
+q1 = np.array(ABOVE_MARKER)
+q2 = np.array(ON_MARKER)
+
+waypointsq0q1 = np.array([[q0[i], q1[i]]for i in range(7)])
+waypointsq1q2 = np.array([[q1[i], q2[i]]for i in range(7)])
+# print(waypoints)
+
+discritized_points_q0q1 = utils.checkpoint_lerp(waypointsq0q1, times)
+discritized_points_q1q2 = utils.checkpoint_lerp(waypointsq1q2, times)
+# print(discritized_points[0, :])
+# print("break")
+# print(discritized_points.T[:, 0])
+# print(discritized_points.T[:, 1])
+
+if __name__ == '__main__':
+    fa = FrankaArm()
+    TrajectoryFollower.follow_joint_trajectory(discritized_points_q0q1)
+    TrajectoryFollower.follow_joint_trajectory(discritized_points_q1q2)
+    fa.close_gripper()
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,136 @@
 import numpy as np
+import math
+
+
+
+def trajectory_trap_vel(waypoints, times, frequency=(1/0.02), duty_cycle=0.25):
+    """
+    Returns a matrix of joint angles, where each column represents a single
+    timestamp. These joint angles form trapezoidal velocity trajectory segments,
+    hitting waypoints[:, i] at times[i].
+
+    Args:
+    waypoints (np.array): Matrix of waypoints; each column represents a single
+                          waypoint in joint space, and each row represents a particular joint.
+    times (np.array): Row vector that indicates the time each of the waypoints should
+                      be reached. The number of columns should equal the number of waypoints,
+                      and should be monotonically increasing.
+    frequency (float): The control frequency at which this trajectory should be played,
+                       and therefore the number of columns per second of playback.
+    duty_cycle (float): The duty cycle for the trapezoidal velocity profile.
+
+    Returns:
+    np.array: Matrix of joint angles forming the trajectory.
+    """
+    # Number of joints
+    num_joints = waypoints.shape[0]
+    # Number of waypoints
+    num_waypoints = waypoints.shape[1]
+    # Number of segments between waypoints
+    num_segments = num_waypoints - 1
+
+    if times.shape != (1, num_waypoints):
+        print(times.shape, num_waypoints)
+        raise ValueError('Size of times vector is incorrect!')
+
+    if num_waypoints < 2:
+        raise ValueError('Insufficient number of waypoints.')
+
+    if not isinstance(frequency, (int, float)) or frequency < 5:
+        raise ValueError('Invalid control frequency (must be at least 5Hz)')
+
+    if duty_cycle < 0 or duty_cycle > 0.5:
+        raise ValueError('Invalid duty cycle!')
+
+    # Calculate number of points per segment
+    num_points_per_segment = []
+    for segment in range(num_segments):
+        dt = times[0, segment + 1] - times[0, segment]
+        num_points_per_segment.append(int(dt * frequency))
+
+    # Pre-allocate trajectory matrix
+    trajectory = np.zeros((num_joints, int(np.sum(num_points_per_segment))))
+
+    # Fill in trajectory segment-by-segment
+    segment_start_point = 0
+    for segment in range(num_segments):
+        points_in_segment = num_points_per_segment[segment]
+        segment_end_point = segment_start_point + points_in_segment
+
+        num_ramp_points = int(duty_cycle * points_in_segment)
+        ramp_time = (times[0, segment + 1] - times[0, segment]) * duty_cycle
+
+        # --------------- BEGIN STUDENT SECTION ----------------------------------
+        # TODO: Calculate the maximum velocity for this segment
+        vm = (waypoints[:,segment+1]-waypoints[:,segment])/(times[:,segment+1]-times[:,segment]-ramp_time)
+        
+        # TODO: Fill in the points for this segment of the trajectory
+        # You need to implement the trapezoidal velocity profile here
+        # Hint: Use three phases: ramp up, constant velocity, and ramp down
+
+        # Example structure (you need to fill in the correct calculations):
+        for joint in range(num_joints):
+            q0 = waypoints[joint, segment]
+            qf = waypoints[joint, segment + 1]
+            v_max = vm[joint]
+            t_b = (points_in_segment-num_ramp_points-1)/frequency
+            t_a = num_ramp_points/frequency
+            t0 = t_a-ramp_time
+            tf=t_b+ramp_time
+
+            for i in range(points_in_segment):
+                t = i / frequency
+                if i <= num_ramp_points:
+                    q = q0 + 0.5*v_max/ramp_time*(t-t0)**2
+                    # q=0
+                elif i > points_in_segment - num_ramp_points:
+                    q_ta = q0+0.5*v_max/ramp_time*(t_a-t0)**2
+                    q_tb = q_ta + v_max*(t_b - t_a)
+                    q = q_tb - 0.5*v_max/ramp_time*(tf**2 - 2*tf*t + t**2 - ramp_time**2)
+                    # q=0
+                    
+                    # q0+0.5*v_max/ramp_time*(num_ramp_points/frequency)**2 + v_max*(((points_in_segment-num_ramp_points)/frequency)-(num_ramp_points/frequency))-0.5*v_max/ramp_time*(tf**2-2*tf*t+t**2-ramp_time**2)
+                else:
+                    q_ta = q0+0.5*v_max/ramp_time*(t_a-t0)**2
+                    q =  q_ta + v_max*(t-t_a)
+                    # q=0
+
+
+                # print("q",q)
+                trajectory[joint, segment_start_point + i] = q
+
+        # --------------- END STUDENT SECTION ------------------------------------
+
+        segment_start_point += points_in_segment
+
+    return trajectory
+
+def checkpoint_lerp(waypoints, times):
+    # q0 and q1 are 1x7 matricies 
+    # f contains our constrained interpolation
+
+    f = trajectory_trap_vel(waypoints, times)
+
+    print(f)
+    print(f.shape)
+
+    # discrete_points = np.zeros()
+    # print(discrete_points)
+    # print(discrete_points.shape)
+
+
+    # q0 = waypoints[0]
+    # print(q0.shape)
+    # print(q0)
+    # q1 = waypoints[1]
+
+    # for i in range(7):
+    #     x = q0[i] * (1-f) + q1[i] * f
+    #     print("SHAPE OF THIS STUFF: ",x.shape)
+    #     discrete_points[i, :] = q0[i] * (1-f) + q1[i] * f
+
+    # returns a matrix that is nx7
+    return f
 
 def _slerp(self, q0, q1, t):
     """Spherical Linear Interpolation between quaternions"""
